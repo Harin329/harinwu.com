59cf28a69bc4dea373348f041f0584ec1ed1fe17
Harin <master.harin@gmail.com>
2023-11-22 19:32:18 -0800

    Cleanup Custom HTML Web Components

--- harinwu.com/components/footer.js
+++ harinwu.com/components/footer.js
@@ -1,46 +1,60 @@
-//Footer
+// Footer
+// via HTML Web Component Custom Elements
 
 class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
   connectedCallback() {
+    const style = document.createElement("style");
+    style.textContent = `
+      .iconStyle1 {
+        height: 30px;
+        margin: 10px;
+      }
+    `;
     this.innerHTML = ` 
-    <footer class="page_footer">
-    <p>
-      <a href="mailto:harinwu99@gmail.com">
-        <div>
-          <img src="../../images/Gmail.png" height="30px" style="margin: 10px;" />
-        </div>
-      </a>
+    <!-- Footer -->
+      <footer class="page_footer">
+        <p>
+          <a href="mailto:harinwu99@gmail.com">
+            <div>
+              <img src="../../images/Gmail.png" class="iconStyle1"/>
+            </div>
+          </a>
 
-      <a href="https://github.com/Harin329">
-        <div>
-          <img src="../../images/Github.png" height="30px" style="margin: 10px;" />
-        </div>
-      </a>
+          <a href="https://github.com/Harin329">
+            <div>
+              <img src="../../images/Github.png" class="iconStyle1" />
+            </div>
+          </a>
 
-      <a href="https://www.linkedin.com/in/hao-harin-wu-28a59a160">
-        <div>
-          <img src="../../images/LinkedIn.png" height="30px" style="margin: 10px;" />
-        </div>
-      </a>
+          <a href="https://www.linkedin.com/in/hao-harin-wu-28a59a160">
+            <div>
+              <img src="../../images/LinkedIn.png" class="iconStyle1" />
+            </div>
+          </a>
 
-      <a href="https://www.facebook.com/harin.wu">
-        <div>
-          <img src="../../images/Facebook.png" height="35px" style="margin: 5px;" />
-        </div>
-      </a>
-      <a href="https://www.instagram.com/harinwu">
-        <div>
-          <img src="../../images/Instagram.png" height="50px" style="margin-left: 5px;" />
-        </div>
-      </a>
-      <a href="https://twitter.com/harinhaowu">
-        <div>
-          <img src="../../images/Twitter.svg" height="55px" style="margin: 0px;" />
-        </div>
-      </a>
-      </p>
+          <a href="https://www.facebook.com/harin.wu">
+            <div>
+              <img src="../../images/Facebook.png" height="35px" style="margin: 5px;" />
+            </div>
+          </a>
+          <a href="https://www.instagram.com/harinwu">
+            <div>
+              <img src="../../images/Instagram.png" height="50px" style="margin-left: 5px;" />
+            </div>
+          </a>
+          <a href="https://twitter.com/harinhaowu">
+            <div>
+              <img src="../../images/Twitter.svg" height="55px"/>
+            </div>
+          </a>
+        </p>
       </footer>
-      `;
+    `;
+    this.appendChild(style);
   }
 }
 
--- harinwu.com/components/header.js
+++ harinwu.com/components/header.js
@@ -1,32 +1,43 @@
-//Header
+// Header
+// via HTML Web Component Custom Elements
 
 class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
   connectedCallback() {
     this.innerHTML = ` 
     <!-- Header -->
-    
       <p>
-        <a href="../../">Home</a><span class="divider"></span> <a href="../../goals/">About Me</a><span
-          class="divider"></span> <a onclick="document.getElementById('downloadResume').style.display='block'"
-          style="cursor: pointer;">Resume</a>
+        <a href="../../">Home</a>
+        <span class="divider"/>
+        <a href="../../goals/">About Me</a>
+        <span class="divider"/>
+        <a onclick="document.getElementById('downloadResume').style.display='block'" style="cursor: pointer;">
+          Resume
+        </a>
       </p>
-      
-      <!-- Resume Modal -->
+    <!-- Resume Modal -->
       <div id="downloadResume" class="w3-modal w3-animate-opacity">
         <div class="w3-modal-content" style="padding:32px">
           <div class="w3-container w3-white">
             <span onclick="document.getElementById('downloadResume').style.display='none'" class="close">&times;</span>
             <h1 class="w3-large">Download Resume Below</h1>
-            <p class="w3-medium"><a href="../../Hao_Wu_Resume.docx" target="_blank" class="w3-hover-text-red">.DOCX
-                Download</a>
+            <p class="w3-medium">
+              <a href="../../Hao_Wu_Resume.docx" target="_blank" class="w3-hover-text-red">.DOCX
+                Download
+              </a>
             </p>
-            <p class="w3-medium"><a href="../../Hao_Wu_Resume.pdf" target="_blank" class="w3-hover-text-red">.PDF
-                Download</a>
+            <p class="w3-medium">
+              <a href="../../Hao_Wu_Resume.pdf" target="_blank" class="w3-hover-text-red">.PDF
+                Download
+              </a>
             </p>
           </div>
         </div>
       </div>
-        `;
+    `;
   }
 }
 
